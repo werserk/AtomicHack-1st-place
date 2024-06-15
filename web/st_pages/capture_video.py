aa5f0e594b3f0d3bde54a9eba0973a41fa350978
werserk <werserk@inbox.ru>
2024-06-15 20:07:53 +0300

    back + front

--- web/st_pages/capture_video.py
+++ web/st_pages/capture_video.py
@@ -5,6 +5,8 @@ import requests
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
+frame_counter = -1
+
 
 def process_frame_via_api(image: np.ndarray) -> np.ndarray:
     """
@@ -24,14 +26,32 @@ def process_frame_via_api(image: np.ndarray) -> np.ndarray:
         raise Exception("Error processing frame via API")
 
 
+def process_frame_native(image: np.ndarray) -> np.ndarray:
+    """
+    Simulates the neuro module's image processing. Here, it simply inverts the image colors.
+
+    :param image: The original image as a numpy array.
+    :return: Processed image with inverted colors.
+    """
+    resized_image = cv2.resize(image, (640, 640))
+    inverted_image = cv2.bitwise_not(image)
+    result_image = cv2.resize(inverted_image, (resized_image.shape[1], resized_image.shape[0]))
+    return result_image
+
+
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
+
+    resized_image = cv2.resize(image, (640, 640))
+
     try:
-        processed_image = process_frame_via_api(image)
+        processed_image = process_frame_native(resized_image)
     except Exception as e:
         st.error(f"Error: {e}")
         return frame
-    return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
+
+    return av.VideoFrame.from_ndarray(cv2.resize(processed_image, (image.shape[1], image.shape[0])),
+                                      format="bgr24")  # Resize back to original resolution
 
 
 def capture_video_page():
